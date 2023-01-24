09a7f64e6353f5a7f22e19b980b198a74690f16a
Zachary Sakowitz <zsakowitz@gmail.com>
2023-01-24 07:00:19 -0800

    Update assignment pages to new DB layer

--- src/routes/assignment/[id]/+page.server.ts
+++ src/routes/assignment/[id]/+page.server.ts
@@ -1,14 +1,42 @@
 import { unwrapOr500 } from "$lib/result"
-import * as AssignmentStatus from "$lib/server/assignment-status"
+import { AssignmentStatus } from "$lib/server/assignment-status"
 import * as Extract from "$lib/server/extract"
 import { sanitize } from "$lib/server/sanitize"
 import { error } from "@sveltejs/kit"
 import type { Actions, PageServerLoad } from "./$types"
 
 export const load = (async ({ locals: { account }, params }) => {
-  const status = unwrapOr500(await AssignmentStatus.get({ id: params.id }))
+  const myId = unwrapOr500(await account.id())
 
-  if (status.assigneeId != account.id) {
+  const status = unwrapOr500(
+    await new AssignmentStatus({ id: params.id }).select({
+      assigneeId: true,
+      assignment: {
+        select: {
+          attachments: true,
+          description: true,
+          groups: {
+            select: {
+              id: true,
+              title: true,
+            },
+            where: { memberIds: { has: myId } },
+          },
+          points: true,
+          title: true,
+        },
+      },
+      attachments: true,
+      body: true,
+      due: true,
+      exempt: true,
+      score: true,
+      submitted: true,
+      teacherComment: true,
+    })
+  )
+
+  if (status.assigneeId != myId) {
     throw error(
       503,
       "It looks like you're trying to see someone else's assignment! Well, okay. I uploaded their data at zsnout.com/?rr, just for you."
@@ -24,70 +52,90 @@ const descriptionExtractor = Extract.fromRequest(
 
 export const actions = {
   async draft({ locals: { account }, params, request }) {
-    const status = unwrapOr500(await AssignmentStatus.get({ id: params.id }))
+    const myId = unwrapOr500(await account.id())
 
-    if (status.assigneeId != account.id) {
+    const status = new AssignmentStatus({ id: params.id })
+
+    const data = unwrapOr500(
+      await status.select({
+        assigneeId: true,
+        submitted: true,
+      })
+    )
+
+    if (data.assigneeId != myId) {
       throw error(
         503,
         "Are you trying to change the contents of someone else's assignment? Shame."
       )
     }
 
-    if (status.submitted) {
+    if (data.submitted) {
       throw error(409, "You can't change an assignment after it's submitted!")
     }
 
     const { body } = await descriptionExtractor(request)
 
-    unwrapOr500(
-      await AssignmentStatus.update(
-        { id: params.id },
-        { body: sanitize(body || "") }
-      )
-    )
+    unwrapOr500(await status.update({ body: sanitize(body || "") }))
   },
   async submit({ locals: { account }, params, request }) {
-    const status = unwrapOr500(await AssignmentStatus.get({ id: params.id }))
+    const myId = unwrapOr500(await account.id())
 
-    if (status.assigneeId != account.id) {
+    const status = new AssignmentStatus({ id: params.id })
+
+    const data = unwrapOr500(
+      await status.select({
+        assigneeId: true,
+        submitted: true,
+      })
+    )
+
+    if (data.assigneeId != myId) {
       throw error(
         503,
         "Are you trying to change the contents of someone else's assignment? Shame."
       )
     }
 
-    if (status.submitted) {
+    if (data.submitted) {
       throw error(409, "You can't change an assignment after it's submitted!")
     }
 
     const { body } = await descriptionExtractor(request)
 
     unwrapOr500(
-      await AssignmentStatus.update(
-        { id: params.id },
-        { body: sanitize(body || ""), submitted: new Date() }
-      )
+      await status.update({
+        body: sanitize(body || ""),
+        submitted: new Date(),
+      })
     )
   },
   async unsubmit({ locals: { account }, params }) {
-    const status = unwrapOr500(await AssignmentStatus.get({ id: params.id }))
+    const myId = unwrapOr500(await account.id())
 
-    if (status.assigneeId != account.id) {
+    const status = new AssignmentStatus({ id: params.id })
+
+    const data = unwrapOr500(
+      await status.select({
+        assigneeId: true,
+        submitted: true,
+      })
+    )
+
+    if (data.assigneeId != myId) {
       throw error(
         503,
         "Why are you trying to unsubmit someone else's assignment?"
       )
     }
 
-    if (!status.submitted) {
+    if (!data.submitted) {
       throw error(
         409,
         "I don't think I can unsubmit an assignment that isn't submitted yet."
       )
     }
 
-    unwrapOr500(
-      await AssignmentStatus.update({ id: params.id }, { submitted: null })
-    )
+    unwrapOr500(await status.update({ submitted: null }))
   },
 } satisfies Actions
