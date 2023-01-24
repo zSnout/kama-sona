f5bfbe107695d08adc072eb7acf59a87f2e4032b
Zachary Sakowitz <zsakowitz@gmail.com>
2023-01-24 12:16:54 -0800

    Make create/group work with the new DB layer

--- src/routes/create/group/+page.server.ts
+++ src/routes/create/group/+page.server.ts
@@ -1,6 +1,6 @@
 import { unwrapOr500 } from "$lib/result"
 import { extractData } from "$lib/server/extract"
-import * as Group from "$lib/server/group"
+import { Group } from "$lib/server/group"
 import { redirect } from "@sveltejs/kit"
 import type { Actions } from "./$types"
 
@@ -8,12 +8,18 @@ export const actions = {
   async default({ locals: { account }, request }) {
     const { title } = await extractData(request, ["title"] as const)
 
-    const { id } = unwrapOr500(
+    const group = unwrapOr500(
       await Group.create({
-        group: { title: title.trim().slice(0, 32) },
-        manager: { id: account.id },
+        title: title.trim().slice(0, 32),
+        managers: {
+          connect: {
+            id: unwrapOr500(await account.id()),
+          },
+        },
       })
     )
+
+    const { id } = unwrapOr500(await group.select({ id: true }))
 
     throw redirect(302, `/group/${id}`)
   },
