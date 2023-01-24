236cc859bd6f6762bb630bd6bd4f8d4af5f107f1
Zachary Sakowitz <zsakowitz@gmail.com>
2023-01-24 07:05:47 -0800

    Update log in to new DB layer

--- src/routes/log-in/+page.server.ts
+++ src/routes/log-in/+page.server.ts
@@ -1,9 +1,8 @@
 import { PUBLIC_KS_BYPASS_LOGIN } from "$env/static/public"
 import { unwrapOr500 } from "$lib/result"
-import * as Account from "$lib/server/account"
+import { Account, AccountList } from "$lib/server/account"
 import { extractData } from "$lib/server/extract"
-import * as MagicLink from "$lib/server/magic-link"
-import * as Session from "$lib/server/session"
+import { MagicLink } from "$lib/server/magic-link"
 import { error, redirect } from "@sveltejs/kit"
 import type { Actions } from "./$types"
 
@@ -15,7 +14,11 @@ export const actions = {
       throw error(400, "Your email address is invalid.")
     }
 
-    if (unwrapOr500(await Account.count({ email })) == 0) {
+    const accountsWithEmail = unwrapOr500(
+      await new AccountList({ email }).count()
+    )
+
+    if (accountsWithEmail == 0) {
       await new Promise((resolve) =>
         setTimeout(resolve, Math.random() * 1000 + 500)
       )
@@ -24,8 +27,8 @@ export const actions = {
     }
 
     if (PUBLIC_KS_BYPASS_LOGIN == "true") {
-      const account = unwrapOr500(await Account.get({ email }))
-      const { code } = unwrapOr500(await Session.get({ forId: account.id }))
+      const session = unwrapOr500(await new Account({ email }).session())
+      const { code } = unwrapOr500(await session.select({ code: true }))
 
       cookies.set("session", code, {
         path: "/",
@@ -35,8 +38,11 @@ export const actions = {
       throw redirect(303, "/")
     }
 
-    unwrapOr500(await MagicLink.create({ email }))
-    unwrapOr500(await MagicLink.send({ email }))
+    const link = unwrapOr500(
+      await MagicLink.create({ for: { connect: { email } } })
+    )
+
+    unwrapOr500(await link.sendVerificationEmail())
 
     return { email }
   },
