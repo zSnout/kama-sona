b32c5303b75b49a659bdf151a41a420f061c2b11
Zachary Sakowitz <zsakowitz@gmail.com>
2023-01-24 07:53:40 -0800

    Switch sign up pages to new DB layer

--- src/routes/sign-up/+page.server.ts
+++ src/routes/sign-up/+page.server.ts
@@ -4,7 +4,7 @@ import {
 } from "$env/static/public"
 import { unwrapOr500 } from "$lib/result"
 import { extractData } from "$lib/server/extract"
-import * as UnverifiedAccount from "$lib/server/unverified-account"
+import { UnverifiedAccount } from "$lib/server/unverified-account"
 import { error } from "@sveltejs/kit"
 import type { Actions, PageServerLoad } from "./$types"
 
@@ -12,7 +12,7 @@ export const load = (() => {
   if (PUBLIC_KS_ENABLE_SIGN_UP != "true") {
     throw error(
       503,
-      `${PUBLIC_KS_APP_NAME} does not allow signing up for a new account.`
+      `${PUBLIC_KS_APP_NAME} does not allow signing up for new accounts.`
     )
   }
 }) satisfies PageServerLoad
@@ -25,7 +25,7 @@ export const actions = {
     ] as const)
 
     const created = unwrapOr500(await UnverifiedAccount.create({ email, name }))
-    unwrapOr500(await UnverifiedAccount.sendVerification(created))
+    unwrapOr500(await created.sendVerificationEmail())
 
     return { email, name }
   },
